2908295d63f990271322d080ab35bcb51f9fdbc2
felipendelicia <delicia4581@gmail.com>
2022-07-20 20:07:01 -0300

    adding electron-reload

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
 
+if (process.env.NODE_ENV !== 'production'){
+    require('electron-reload')(__dirname, {
+    })
+}
+
 let mainWindow
 
 const mainWindowOptions = {
     minHeight: 400,
     minWidth: 600,
-    title:'Maintenance software'
+    title: 'Maintenance software'
 }
 
 app.on('ready', ()=>{
