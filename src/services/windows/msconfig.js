c2dbcb9deb49abc1e5b3cc74d1de0dd61ac8d556
felipendelicia <delicia4581@gmail.com>
2022-07-22 14:51:59 -0300

    adding msconfig

---
+++ src/services/windows/msconfig.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+
+function msconfig() {
+  exec("msconfig", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+
+    console.log(`stdout:\n${stdout}`);
+  });
+}
